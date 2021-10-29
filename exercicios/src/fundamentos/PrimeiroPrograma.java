e7de58b74f796f16c95047c3287a5abaeefae903
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-28 22:42:21 -0300

    First Commit

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -3,5 +3,6 @@ package fundamentos;
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Pelo Sport tudo!");
+		System.out.println("Pelo Sport tudo!");
 	}
 }
