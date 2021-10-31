8f85a2d1f27dda5732d540eb6c24a8769346ac95
Eraldo Vicente <eraldovicente2@gmail.com>
2021-10-31 16:34:22 -0300

    Comentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,23 @@
 package fundamentos;
 
 public class PrimeiroPrograma {
+	
+	/**
+	 * Essa classe representa
+	 * 
+	 * @author Eraldo Vicente
+	 * @since JDK
+	 * @see
+	 */
 	public static void main(String[] args) {
+		
+		// Uma sentença de código com ;
 		System.out.println("Pelo Sport tudo!");
+		
+		/**
+		 * Linha 1 
+		 * Linha 2
+		 */
 		System.out.println("Pelo Sport tudo!");
 	}
 }
