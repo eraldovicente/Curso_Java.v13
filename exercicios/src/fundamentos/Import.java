414e3899e631afae58e84e9748f6262ffaa9b270
Eraldo Vicente <eraldovicente2@gmail.com>
2021-11-01 18:56:56 -0300

    Tipo String

--- exercicios/src/fundamentos/Import.java
+++ exercicios/src/fundamentos/Import.java
@@ -1,5 +1,14 @@
 package fundamentos;
 
-public class Import {
+import java.util.Date;
 
+public class Import {
+	
+	public static void main(String[] args) {
+		String s = "Bom dia!";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+	}
 }
