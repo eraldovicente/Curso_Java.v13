90c1ecd8ce0d562f9f7ace607e67e1ce0cd68ac4
Eraldo Vicente <eraldovicente2@gmail.com>
2021-11-23 22:22:10 -0300

    Desafio herança #02

--- exercicios/src/oo/heranca/desafio/Carro.java
+++ exercicios/src/oo/heranca/desafio/Carro.java
@@ -1,11 +1,21 @@
 package oo.heranca.desafio;
 
 public class Carro {
-
+	
+	final int VELOCIDADE_MAXIMA;
 	int velocidadeAtual;
+	int delta = 5;
+	
+	Carro(int velecidadeMaxima) {
+		VELOCIDADE_MAXIMA = velecidadeMaxima;
+	}
 	
 	void acelerar() {
-		velocidadeAtual += 5;
+		if(velocidadeAtual + delta > VELOCIDADE_MAXIMA) {
+			velocidadeAtual = VELOCIDADE_MAXIMA;
+		} else {
+			velocidadeAtual += delta;			
+		}
 	}
 	
 	void frear() {
