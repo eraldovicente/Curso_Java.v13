417494ef705cc9dd9e1110c7e3a0223b58f9565c
Eraldo Vicente <eraldovicente2@gmail.com>
2021-11-24 23:01:22 -0300

    Classe abstrata #02

--- exercicios/src/oo/polimorfismo/Jantar.java
+++ exercicios/src/oo/polimorfismo/Jantar.java
@@ -4,10 +4,9 @@ public class Jantar {
 	
 	public static void main(String[] args) {
 		
-		Pessoa convidado = new Pessoa(99.65);
-		
+		Pessoa convidado = new Pessoa(99.65);		
 		Arroz ingrediente1 = new Arroz(0.2);
-		Feijao ingrediente2 = new Feijao(0.1);
+		Comida ingrediente2 = new Feijao(0.1);
 		
 		System.out.println(convidado.getPeso());
 		
