6c5545d98f454dc540acab57420950f78361846d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-11-25 16:05:38 -0300

    Interfaces funcionais #02

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -4,4 +4,12 @@ package lambdas;
 public interface Calculo {
 	
 	public abstract double executar(double a, double b);
+	
+	default String legal() {
+		return "legal";
+	}
+	
+	static String muitoLegal() {
+		return "muito legal";
+	}
 }
