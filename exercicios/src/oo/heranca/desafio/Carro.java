31d8527e015041006811773c207cdb18285c5c40
Eraldo Vicente <eraldovicente2@gmail.com>
2021-11-24 21:24:45 -0300

    Interface #01

--- exercicios/src/oo/heranca/desafio/Carro.java
+++ exercicios/src/oo/heranca/desafio/Carro.java
@@ -4,17 +4,17 @@ public class Carro {
 	
 	public final int VELOCIDADE_MAXIMA;
 	protected int velocidadeAtual;
-	protected int delta = 5;
+	private int delta = 5;
 	
 	protected Carro(int velecidadeMaxima) {
 		VELOCIDADE_MAXIMA = velecidadeMaxima;
 	}
 	
 	public void acelerar() {
-		if(velocidadeAtual + delta > VELOCIDADE_MAXIMA) {
+		if(velocidadeAtual + getDelta() > VELOCIDADE_MAXIMA) {
 			velocidadeAtual = VELOCIDADE_MAXIMA;
 		} else {
-			velocidadeAtual += delta;			
+			velocidadeAtual += getDelta();			
 		}
 	}
 	
@@ -28,5 +28,13 @@ public class Carro {
 	
 	public String toString() {
 		return "Velocidade atual é " + velocidadeAtual + "Km/h.";
+	}
+
+	public int getDelta() {
+		return delta;
+	}
+
+	public void setDelta(int delta) {
+		this.delta = delta;
 	}
 }
