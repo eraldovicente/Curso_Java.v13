c4839804a3fa565e5d7edb37d5890fb556c15531
Eraldo Vicente <eraldovicente2@gmail.com>
2021-11-26 18:46:01 -0300

    Desafio Map - resposta

--- exercicios/src/streams/DesafioMap.java
+++ exercicios/src/streams/DesafioMap.java
@@ -2,6 +2,8 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class DesafioMap {
 	
@@ -15,5 +17,15 @@ public class DesafioMap {
 		 * 3. Converter de volta para inteiro => "011" => 3		  
 		 */
 		
+		UnaryOperator<String> inverter =
+				s -> new StringBuilder(s).reverse().toString();
+		Function<String, Integer> binarioParaInt =
+				s -> Integer.parseInt(s, 2);
+		
+		nums.stream()
+			.map(Integer::toBinaryString)
+			.map(inverter)
+			.map(binarioParaInt)
+			.forEach(System.out::println);
 	}
 }
