c1cb377ec14416b7d1f27ec7b1e547d7940f17ee
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-07 15:57:36 -0300

    Consultar Estados

---
+++ banco-relacional/consultarEstados.sql
@@ -0,0 +1,19 @@
+SELECT 
+    *
+FROM
+    estados;
+
+SELECT 
+    Sigla, nome AS 'Nome do Estado'
+FROM
+    estados
+WHERE
+    regiao = 'Sul';
+    
+SELECT 
+    nome, regiao
+FROM
+    estados
+WHERE
+    populacao >= 10
+ORDER BY populacao DESC;
