e400ed9ab7c7dc38fa4ad67b76634f12b1c136f0
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-08 21:25:47 -0300

    Consultar múltiplas tabelas (join)

--- banco-relacional/inserirCidades.sql
+++ banco-relacional/inserirCidades.sql
@@ -1,11 +1,11 @@
 INSERT INTO cidades (nome, area, estado_id) 
-VALUES ('Igarassu', 305.56, 23);
+VALUES ('Igarassu', 305.56, 17);
 
 INSERT INTO cidades (nome, area, estado_id) 
 VALUES ('Campinas', 795, 25);
 
 INSERT INTO cidades (nome, area, estado_id) 
-VALUES ('Niterói', 133.9, 25);
+VALUES ('Niterói', 133.9, 19);
 
 INSERT INTO cidades (nome, area, estado_id) 
 VALUES ('Caruaru', 920.6, (SELECT id FROM estados WHERE sigla = 'PE'));
