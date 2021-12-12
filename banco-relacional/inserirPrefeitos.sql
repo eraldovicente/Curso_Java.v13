be61cb3e03480e995b0e6f80871632cb8de29158
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-11 23:19:24 -0300

    Inserir prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,14 @@
+select * from cidades;
+
+insert into prefeitos
+	(nome, cidade_id)
+values
+	('Rodrigo Neves', 1),
+    ('Raquel Lyra', 3),
+    ('Eraldo Vicente', null);
+    
+insert into prefeitos
+	(nome, cidade_id)
+values
+	('Rafael Greca', null);
+    
