c92e6a66f00f52e46945be9a66c0e8bd9c7ff1bd
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-19 14:10:05 -0300

    Módulos e reflection

--- app-calculo/src/br/com/cod3r/app/calculo/Calculadora.java
+++ app-calculo/src/br/com/cod3r/app/calculo/Calculadora.java
@@ -5,10 +5,17 @@ import br.com.cod3r.app.logging.Logger;
 
 public class Calculadora {
 	
+	private String id = "abc";
+	
 	private OperacoesAritmeticas opAritmeticas = new OperacoesAritmeticas();
 
 	public double soma(double... nums) {
 		Logger.info("Somando...");
 		return opAritmeticas.soma(nums);
 	}
+
+	public String getId() {
+		return id;
+	}
+	
 }
--- app-financeiro/src/br/com/cod3r/app/financeiro/Teste.java
+++ app-financeiro/src/br/com/cod3r/app/financeiro/Teste.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.app.financeiro;
 
+import java.lang.reflect.Field;
+
 import br.com.cod3r.app.calculo.Calculadora;
 import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
 
@@ -12,6 +14,19 @@ public class Teste {
 		
 		OperacoesAritmeticas op = new OperacoesAritmeticas();
 		System.out.println(op.soma(4, 5, 6));
+		
+		try {
+			Field fieldId = Calculadora.class.getDeclaredFields()[0];
+			fieldId.setAccessible(true);
+			fieldId.set(calc, "def");			
+			fieldId.setAccessible(false);
+			
+			System.out.println(calc.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println();
 
 	}
 }
