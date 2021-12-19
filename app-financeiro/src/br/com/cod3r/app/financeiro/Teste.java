dae3cc506f63301374bd765dfd65027ff44e2dff
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-19 15:52:06 -0300

    Interface vs implementação

--- app-financeiro/src/br/com/cod3r/app/financeiro/Teste.java
+++ app-financeiro/src/br/com/cod3r/app/financeiro/Teste.java
@@ -9,24 +9,29 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		
-		Calculadora calc = new Calculadora();		
-		System.out.println(calc.soma(2, 3, 4));
+		Calc c1 = new CalcImpl1(); 
+		Calc c2 = new CalcImpl2();
 		
-		OperacoesAritmeticas op = new OperacoesAritmeticas();
-		System.out.println(op.soma(4, 5, 6));
+		System.out.println(c1.soma(1, 2, 3));
+		System.out.println(c2.soma(1, 2, 3));
 		
-		try {
-			Field fieldId = Calculadora.class.getDeclaredFields()[0];
-			fieldId.setAccessible(true);
-			fieldId.set(calc, "def");			
-			fieldId.setAccessible(false);
-			
-			System.out.println(calc.getId());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println();
+//		Calculadora calc = new Calculadora();		
+//		System.out.println(calc.soma(2, 3, 4));
+//		
+//		OperacoesAritmeticas op = new OperacoesAritmeticas();
+//		System.out.println(op.soma(4, 5, 6));
+//		
+//		try {
+//			Field fieldId = Calculadora.class.getDeclaredFields()[0];
+//			fieldId.setAccessible(true);
+//			fieldId.set(calc, "def");			
+//			fieldId.setAccessible(false);
+//			
+//			System.out.println(calc.getId());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
 
 	}
 }
