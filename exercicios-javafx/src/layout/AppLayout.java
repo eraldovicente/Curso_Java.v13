1843ab54fd54a4ba44e0eac581bcfe419e41f86c
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-26 13:36:02 -0300

    TilePane

--- exercicios-javafx/src/layout/AppLayout.java
+++ exercicios-javafx/src/layout/AppLayout.java
@@ -13,11 +13,12 @@ public class AppLayout extends Application {
 		
 		Parent raiz = null;
 		
-		raiz = new TesteAnchorPane();
-		raiz = new TesteBorderPane();
-		raiz = new TesteFlowPane();
-		raiz = new TesteGridPane();
-		raiz = new TesteStackPane();
+//		raiz = new TesteAnchorPane();
+//		raiz = new TesteBorderPane();
+//		raiz = new TesteFlowPane();
+//		raiz = new TesteGridPane();
+//		raiz = new TesteStackPane();
+		raiz = new TesteTilePane();
 		
 		Scene principal = new Scene(raiz, 800, 600);
 		primaryStage.setScene(principal);
