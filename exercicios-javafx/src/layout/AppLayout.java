3e110caed9e06e95678540ee9b02d7334d128be9
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-25 23:05:45 -0300

    GridPane

--- exercicios-javafx/src/layout/AppLayout.java
+++ exercicios-javafx/src/layout/AppLayout.java
@@ -16,6 +16,7 @@ public class AppLayout extends Application {
 		raiz = new TesteAnchorPane();
 		raiz = new TesteBorderPane();
 		raiz = new TesteFlowPane();
+		raiz = new TesteGridPane();
 		
 		Scene principal = new Scene(raiz, 800, 600);
 		primaryStage.setScene(principal);
