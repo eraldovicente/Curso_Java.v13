97285e992cb144e2c0cd3cbdb793c3036d0b419a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-26 22:04:50 -0300

    Web service retornando objeto

---
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -0,0 +1,17 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cod3r.exerciciossb.models.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping("/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(6, "Biel", "999.999.999-50");
+	}
+}
