f098ed5f40a90d58346bb48355fc4a716d1abaff
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-26 21:42:10 -0300

    Simulando Erros

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class PrimeiroController {
 	@GetMapping(path = { "/ola", "saudacao" })
 	public String ola() {
 		return "Olá Spring Boot!";
+	}
+	
+	@PostMapping(path = "saudacao")
+	public String saudacao() {
+		return "Olá Spring Boot! (POST)";
 	}
 }
