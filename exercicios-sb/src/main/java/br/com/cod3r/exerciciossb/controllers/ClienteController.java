0f455c6d20b150efb327c50885afde4b7ca92a82
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-27 19:22:16 -0300

    Passando parâmetros para web

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class ClienteController {
 	
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
-		return new Cliente(6, "Biel", "999.999.999-50");
+		return new Cliente(6, "Biel", "999.999.999-99");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "999.999.999-99");
 	}
 }
