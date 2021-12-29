326bcbc6249bedd4b05f3faf27eeffc9dec319e7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-12-28 21:32:07 -0300

    Desafio novo atributos - resposta

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/entities/Produto.java
@@ -4,11 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Transient;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 
 @Entity
 public class Produto {
@@ -18,19 +13,22 @@ public class Produto {
 	private int id;
 
 	private String nome;
-	
-	@Autowired
-	@Transient
-	public ProdutoRepository repository; 
-	
+
+	private double preco;
+
+	private double desconto;
+
 	public Produto() {
-		
+
 	}
-	
-	public Produto(String nome) {
+
+	public Produto(String nome, double preco, double desconto) {
+		super();
 		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -46,4 +44,21 @@ public class Produto {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
+	}
+
 }
